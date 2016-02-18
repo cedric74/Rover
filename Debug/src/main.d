0b2166105c5f9c85f9a97b9630ee42eed8707192
Cedric Toncanier <cedric.toncanier@gmail.com>
2016-02-17 22:03:37 -0500

    config add pthread

--- Debug/src/main.d
+++ Debug/src/main.d
@@ -1,3 +1,6 @@
-src/main.d: ../src/main.c ../src/Lib_main.h
+src/main.d: ../src/main.c ../src/Lib_main.h \
+ ../src/Functions_WatchDog/Functions_WatchDog.h
 
 ../src/Lib_main.h:
+
+../src/Functions_WatchDog/Functions_WatchDog.h:
